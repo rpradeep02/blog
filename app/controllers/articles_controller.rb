9b1dd00c7288360f444be6be1d4f7a1ab81a4612
exit <exit>
2021-03-23 12:23:19 +0530

    added UI with edit show action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,8 @@
 class ArticlesController < ApplicationController
+
+    def index
+      @articles = Article.all
+    end
     def new
         @article = Article.new
     end
@@ -17,6 +21,20 @@ class ArticlesController < ApplicationController
     def show
       @article = Article.find(params[:id])
     end
+    def update
+      @article = Article.find(params[:id])
+      if @article.update(article_params)
+        flash[:notice] = "the article successfully updated"
+        redirect_to article_path(@article)
+      else
+        render 'new'
+      end
+    end
+
+    def edit
+      @article = Article.find(params[:id])
+    end
+    
     private
     def article_params
       params.require(:article).permit(:title, :description)
