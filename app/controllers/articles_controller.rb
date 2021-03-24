67061592ed01a5fe36ea3469094bc57b67bcceee
exit <exit>
2021-03-24 12:54:53 +0530

    add asso with user and articles and debugger

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,9 +7,8 @@ class ArticlesController < ApplicationController
         @article = Article.new
     end
     def create
-      #render plain: params[:article].inspect
       @article = Article.new(article_params)
-
+      @article.user = User.first
       if @article.save
         flash[:success] = "the article successfully created"
         redirect_to article_path(@article)
