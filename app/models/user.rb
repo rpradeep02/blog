bf007feaad33450a4b714db3fd524a96af1f8617
exit <exit>
2021-03-24 13:53:28 +0530

    Add secure password

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,5 @@ class User < ApplicationRecord
             uniqueness: { case_sensitive: false}, 
             length: { maximum: 130},
             format: { with: VALID_EMAIL_REGEX}
+  has_secure_password
 end
