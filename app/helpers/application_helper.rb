8e4e35c85bbef2643f858e159f567d70d9154a63
exit <exit>
2021-03-25 13:50:23 +0530

    Add login and logout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def gravatar_for(user, options = { size: 80})
-    default_url = "#{root_url}'public/images/1.png'"
+    default_url = "#{root_url}/images/1.png"
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     size = options[:size]
     gravatar_url= "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}&d=#{CGI.escape(default_url)}"
