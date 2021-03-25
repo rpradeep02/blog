aa718961555e59d4fb42fe7374ef9d81aa8b312c
exit <exit>
2021-03-25 16:44:00 +0530

    complete authentication

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,54 +1,64 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:edit, :update,:destroy,:show]
-    def index
-      @articles = Article.paginate(page: params[:page], per_page: 5)
-    end
-    def new
-        @article = Article.new
-    end
-    def create
-      @article = Article.new(article_params)
-      @article.user = User.first
-      if @article.save
-        flash[:success] = "the article successfully created"
-        redirect_to article_path(@article)
-      else
-        render 'new'
-      end
-    end
-
-    def update
-      if @article.update(article_params)
-        flash[:success] = "the article successfully updated"
-        redirect_to article_path(@article)
-      else
-        render 'new'
-      end
-    end
-
-    def edit
-     
-    end
-
-    def show
-     
-    end
-
-    def destroy
-      
-      @article.destroy
-      flash[:danger] = "the article successfully deleted"
-      redirect_to articles_path
-    end
+  before_action :require_user, except: [:index, :show]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
   
-
-    private
-
-    def set_article
-      @article = Article.find(params[:id])
+  def index
+    @articles = Article.paginate(page: params[:page], per_page: 5)
+  end
+  def new
+      @article = Article.new
+  end
+  def create
+    @article = Article.new(article_params)
+    @article.user = User.first
+    if @article.save
+      flash[:success] = "the article successfully created"
+      redirect_to article_path(@article)
+    else
+      render 'new'
     end
+  end
 
-    def article_params
-      params.require(:article).permit(:title, :description)
+  def update
+    if @article.update(article_params)
+      flash[:success] = "the article successfully updated"
+      redirect_to article_path(@article)
+    else
+      render 'new'
     end
+  end
+
+  def edit
+    
+  end
+
+  def show
+    
+  end
+
+  def destroy
+    
+    @article.destroy
+    flash[:danger] = "the article successfully deleted"
+    redirect_to articles_path
+  end
+
+
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
+
+  def require_same_user
+    if current_user != @article.user
+      flash[:danger] = "You can only edit or delete your own article"
+      redirect_to root_path
+    end
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,8 @@
 module ApplicationHelper
   def gravatar_for(user, options = { size: 80})
-    default_url = "#{root_url}/images/1.png"
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     size = options[:size]
-    gravatar_url= "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}&d=#{CGI.escape(default_url)}"
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
     image_tag(gravatar_url, alt: user.username, class: "rounded-circle z-depth-2")
   end
 end
